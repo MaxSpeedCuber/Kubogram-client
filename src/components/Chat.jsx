2f1c81bb15e3eaf4b20a8d89b98be60a626af770
MaxSpeedCuber <162599332+MaxSpeedCuber@users.noreply.github.com>
2024-06-14 13:26:04 +0300

    Update Chat.jsx

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -9,7 +9,7 @@ import {io} from "socket.io-client";
 
 
 //Гыыы
-const socket = io.connect("https://kubogram.onrender.com");
+const socket = io.connect("https://kubogram-server.onrender.com");
 
 
 
